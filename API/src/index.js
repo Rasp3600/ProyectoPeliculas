cc65881eca3339e549a6d599e276f522cb22458f
Rasp3600 <127553473+Rasp3600@users.noreply.github.com>
2024-05-27 15:41:22 -0600

    post y put

--- API/src/index.js
+++ API/src/index.js
@@ -6,13 +6,13 @@ const Port = 4000;
 let peliculas = [];
 
 const structure = {
-  id: '',
-  titulo: '',
-  protagonista: '',
-  categoria: '',
-  urlVideo: '',
-  urlImage: ''
-}
+  id: "",
+  titulo: "",
+  protagonista: "",
+  categoria: "",
+  urlVideo: "",
+  urlImage: "",
+};
 
 app.use(cors());
 app.use(express.json());
@@ -21,18 +21,24 @@ app.get("/peliculas", (req, res) => {
   res.json(peliculas);
 });
 
-app.get("/peliculas/:epoca", (req, res) => {
+app.get("/peliculas/:epoca", (req, res) => {});
+
+app.post(`/peliculas`, async (req, res) => {
+  const newpeli = req.body;
+  peliculas.push(newpeli);
+  res.status(201).json("Peli agregada");
 });
 
-app.post("/peliculas", (req, res) => {
+app.put("/peliculas/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedpeli = req.body;
+  const index = peliculas.findIndex((peli) => peli.id == id);
+  peliculas[index] = { ...peliculas[index], ...updatedpeli };
+  res.status(201).json("Peli actualizada");
 });
 
-app.update("/peliculas/:id", (req, res) => {
-});
-
-app.delete("/peliculas/:id", (req, res) => {
-});
+app.delete("/peliculas/:id", (req, res) => {});
 
 app.listen(Port, () => console.log(`server runnin on Port: ${Port}`));
 
-// G5 NO ROMPIO INTER F
+// G5 NO ROMPIO INTER F xdxd
